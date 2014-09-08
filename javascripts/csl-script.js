b06e72e2f6021e86722c855e9529a840095f3529
AJ Singh <ajsingh273@gmail.com>
2014-09-08 19:52:21 -0400

    update

--- javascripts/csl-script.js
+++ javascripts/csl-script.js
@@ -15,14 +15,14 @@
 
 	function stopSpinner(spinner){
 		$('#csl-button').removeClass('search-button-clear');
-		spinner.stop();
+		spinner.stop();	
 	}
 
 	function getCsl(spinner) {
 		var keyword = document.getElementById("csl-keyword").value;
 		var url = "http://api.trade.gov/consolidated_screening_list/search.json?q=" ;
-
-
+	
+		
 		if (keyword.length > 0){
 			url += keyword;
 		}
@@ -45,57 +45,31 @@
 					$('#csl-results').addClass('csl-container');
 					var list = "<p class='results-title'></p>";
 					for (var i=0; i<=results.length-1; i++){
-						var csl = results[i];
-
-						var addresses = '';
-            if (csl.addresses !== null) {
-              for (var i = 0; i < csl.addresses.length; ++i) {
-                var addressString = '';
-                csl_address = csl.addresses[i];
-
-                if (csl_address.address !== null) {
-                  addressString += (csl_address.address + ' ');
-                }
-                if (csl_address.city !== null) {
-                  addressString += (', ' + csl_address.city + ' ');
-                }
-
-                if (csl_address.state !== null) {
-                  addressString += (', ' + csl_address.state + ' ');
-                }
-
-                if (csl_address.postal_code !== null) {
-                  addressString += (', ' + csl_address.postal_code + ' ');
-                }
-
-                if (csl_address.country !== null) {
-                  addressString += (', ' + csl_address.country + ' ');
-                }
-                addresses += (addressString + "<br />");
-              }
-            }
-        
-
-						var alt_names = csl.alt_names;
-						var citizenships = csl.citizenships;
-						var dates_of_birth = csl.dates_of_birth;
-						var entity_number = csl.entity_number;
-						var ids = csl.ids;
-						var name = csl.name;
-						var nationalities = csl.nationalities;
-						var places_of_birth = csl.places_of_birth;
-						var programs = csl.programs;
-						var remarks = csl.remarks;
-						var sdn_type = csl.sdn_type;
-						var source = csl.source;
-						var source_list_url = csl.source_list_url;
-						var title = csl.title;
-
-
-
-
-						list += "<p class='results-legend'>" + "<table> <tr><td>Addresses</td><td>" + addresses + "</td></tr> <tr><td>Alternate Names</td><td>" +  alt_names + "</td></tr> <tr><td>Citizenships</td><td>" +  citizenships + "</td></tr> <tr><td>Dates of Birth</td><td>" +  dates_of_birth + "</td></tr> <tr><td>Entity Number</td><td>" +  entity_number + "</td></tr> <tr><td>IDs</td><td>" +  ids + "</td></tr> <tr><td>Name</td><td>" +  name + "</td></tr> <tr><td>Nationalities</td><td>" +  nationalities + "</td></tr> <tr><td>Places of Birth</td><td>" +  places_of_birth + "</td></tr> <tr><td>Programs</td><td>" +  programs + "</td></tr> <tr><td>Remarks</td><td>" +  remarks + "</td></tr> <tr><td>SDN Type</td><td>" +  sdn_type + "</td></tr> <tr><td>Source</td><td>" +  source + "</td></tr> <tr><td>Source List URL</td><td> <a href=" + source_list_url + " target='_blank'>" + source_list_url +  "</a></td></tr> <tr><td>title</td><td>" +  title + "</td></tr>   </table> </br></br></br></br>"
-
+						
+						var title = results[i].title;
+						var ids = results[i].ids;
+						var name = results[i].name;
+						var alt_names = results[i].alt_names;
+						var entity_number = results[i].entity_number;
+						var sdn_type = results[i].sdn_type;
+						var programs = results[i].programs;
+						var citizenships = results[i].citizenships;
+						var dates_of_birth = results[i].dates_of_birth;
+						var nationalities = results[i].nationalities;
+						var places_of_birth = results[i].places_of_birth;
+						var addresses = results[i].addresses;
+						var source = results[i].source;	
+						var source_list_url = results[i].source_list_url;
+						var remarks = results[i].remarks;
+						
+						
+						
+						
+						
+						
+						
+						 
+					list += "<p class='results-legend'>" + "<table> <tr><td>Addresses</td><td>" + addresses + "</td></tr> <tr><td>Alternate Names</td><td>" +  alt_names + "</td></tr> <tr><td>Citizenships</td><td>" +  citizenships + "</td></tr> <tr><td>Dates of Birth</td><td>" +  dates_of_birth + "</td></tr> <tr><td>Entity Number</td><td>" +  entity_number + "</td></tr> <tr><td>IDs</td><td>" +  ids + "</td></tr> <tr><td>Name</td><td>" +  name + "</td></tr> <tr><td>Nationalities</td><td>" +  nationalities + "</td></tr> <tr><td>Places of Birth</td><td>" +  places_of_birth + "</td></tr> <tr><td>Programs</td><td>" +  programs + "</td></tr> <tr><td>Remarks</td><td>" +  remarks + "</td></tr> <tr><td>SDN Type</td><td>" +  sdn_type + "</td></tr> <tr><td>Source</td><td>" +  source + "</td></tr> <tr><td>Source List URL</td><td> <a href=" + source_list_url + " target='_blank'>" + source_list_url +  "</a></td></tr> <tr><td>title</td><td>" +  title + "</td></tr>   </table> </br></br></br></br>"
 					}
 				}
 				document.getElementById("csl-results").innerHTML = list;
@@ -109,7 +83,7 @@
 		});
 	}
 
-	function main() {
+	function main() { 
 	    $(document).ready(function($) {
 				if (!$("link[href='']").length){
 					$('<script src="javascripts/spin.js" type="text/javascript"></script>').appendTo("head");
@@ -121,7 +95,7 @@
 				container += ('<div id="csl-results" class="csl-container"></div>');
 				document.getElementById('csl-container').innerHTML = container;
 				$('#csl-container').addClass('widget-container');
-				var form = "";
+				var form = "";				
 	      form += ('<p class="widget-title"><h2>Consolidated Screening List</h1></p>');
 				form += ('<div><input class="search-input" type="text" id="csl-keyword" placeholder="" size="40" style="border: 1px solid #03738c; color:#024059">');
 				form += ('<button class="search-button" id="csl-button">SEARCH</button></div>');
